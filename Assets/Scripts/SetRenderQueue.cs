78ffa27162e181f3df51bdb800971a1e4db545b5
nebulimity <koen.kruk@icloud.com>
2026-05-05 21:52:12 -0400

    upload files

---
+++ Assets/Scripts/SetRenderQueue.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SetRenderQueue : MonoBehaviour
+{
+    public int queue = 2002;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.renderQueue = queue;
+    }
+}
